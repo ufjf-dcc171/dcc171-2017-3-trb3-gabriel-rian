5ac5f8182312d48fa4224775a0653ce1cf58545b
Rian18 <rianalves2009.ra@gmail.com>
2017-12-06 20:36:23 -0200

    Criação classe pessoaDAO e pessoaDAOJDBC

---
+++ Trabalho3_Lab3_20173/src/trabalho3_lab3_20173/PessoaDAO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trab_lab3_20173;
+import java.util.List;
+/**
+ *
+ * @author gabrielsantos
+ */
+public interface PessoaDAO {
+    public void cria(Pessoa p) throws Exception;
+    public List<Pessoa> listaTodas() throws Exception;
+}
